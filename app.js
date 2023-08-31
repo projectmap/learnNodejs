530cd8c267717379bf9724ac07dbd637ea28bc95
suren sedai <suren.sedai9@gmail.com>
2023-08-31 12:11:49 +0545

    add server

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "hello world changed",
+    })
+  );
+});
+
+server.listen(3005);
