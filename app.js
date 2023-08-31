a0d9c2b6f900eac333a7e47218317fca0087feec
suren sedai <suren.sedai9@gmail.com>
2023-08-31 12:58:04 +0545

    read from file html

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const http = require("http");
+const fs = require("fs");
+
+const PORT = 3005;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "hello world changed",
-    })
-  );
-});
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/home.html", "utf8", (error, data) => {
+      if (error) throw error;
 
-server.listen(3005);
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/about.html", "utf8", (error, data) => {
+      if (error) throw error;
+
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/post") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/post.html", (error, data) => {
+      if (error) throw error;
+
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h3>Sorry page not found</h3>");
+    res.end();
+  }
+});
+console.log(`Running on localhost:${PORT}`);
+server.listen(PORT);
